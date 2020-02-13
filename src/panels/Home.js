8d26681402af83017ec900c36b689a7901f61ffe
Stegman <hayden.stegman@CHAMPLAIN.EDU>
2020-02-12 19:47:32 -0500

    Updated the HomePage and Styles. Also added a new componet that will be updated to provide the information for a random pet.

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import LikeButton from '../Components/LikeButton.js';
 import DogListing from '../Components/DogListing.js'
+import SingleDogListing from '../Components/SingleDogListing.js'
 import styles from './Home.module.css'
 
 //calls comopents from other files to load into html
@@ -11,17 +12,21 @@ const Home = () => (
         <>
         <div className={styles.grid_container}>
             <div className={styles.left}>
-                <DogListing/>
+                <h1>Types Of Animals</h1>
+				<p>Here you can find all the types of animals that are available in our database</p>
+				<DogListing/>
             </div>
             
             <div className={styles.middle}>
                 <h1>Welcome To PlayDogPlay</h1>
-				<h1>...</h1>
+				<p>PlayDogPlay is a comprehensive pet breed database based on the PetFinder API</p>
 				<LikeButton/> 
             </div>
             
             <div className={styles.right}>
-            
+				<h1>Check Out This Pet</h1>
+				<p>*WIP* - This Section will feature data from a random pet.</p>
+				//<SingleDogListing/>
             </div>
 
             <div className={styles.footer}>
